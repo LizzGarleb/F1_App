a50dac57b30ec1bd8b36a4891db8786c3947a421
LizzGarleb <redscorpion3108@gmail.com>
2023-09-05 13:21:58 -0400

    Initial Commit

---
+++ F1_App/F1_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1_AppApp.swift
+//  F1_App
+//
+//  Created by Lizbeth Garcia on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct F1_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
